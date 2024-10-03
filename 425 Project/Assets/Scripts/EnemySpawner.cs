f87f3529f201b878fa2428d206c8394d47f89ae5
Magicow7 <silasraphaelidis@gmail.com>
2024-10-03 15:02:15 -0400

    merged main scenes

--- 425 Project/Assets/Scripts/EnemySpawner.cs
+++ 425 Project/Assets/Scripts/EnemySpawner.cs
@@ -13,6 +13,7 @@ public class EnemySpawner : MonoBehaviour
         {
             Instantiate(enemy, spawnPositions[Random.Range(0,spawnPositions.Count)].transform.position + modifier, Quaternion.identity);
             Navigation enemyNav = enemy.GetComponent<Navigation>();
+            Debug.Log(player);
             enemyNav.playerObject = player;
         }
     }
