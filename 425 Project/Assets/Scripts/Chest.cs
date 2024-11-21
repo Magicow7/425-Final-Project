69fffdf890520ad0c0d96bd408b23465b4b784c7
Greg <gselez6761@gmail.com>
2024-11-20 20:15:59 -0500

    added my chest to treasure prefab

--- 425 Project/Assets/Scripts/Chest.cs
+++ 425 Project/Assets/Scripts/Chest.cs
@@ -8,33 +8,43 @@ public class Chest : MonoBehaviour
 
     private bool playerInRange = false;
     private bool opened = false;
+    public float chestSpeed = 40f;
+
+    public GameObject lid;
 
     // Start is called before the first frame update
     void Start()
     {
-     
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-       if (Input.GetKeyDown(KeyCode.E) && playerInRange && !opened)
+        Debug.Log("RUNNING UPDATE");
+        if (Input.GetKeyDown(KeyCode.E) && playerInRange && !opened)
         {
             opened = true;
             Debug.Log("chest opened!");
-        }    
+            StartCoroutine(setLid(true));
+        }
     }
 
+    private IEnumerator setLid(bool open)
+    {
+        while (lid.transform.rotation.eulerAngles.x <= 350)
+        {
+            lid.transform.localRotation *= Quaternion.Euler(new Vector3(-50 * Time.deltaTime, 0, 0));
+            yield return null;
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
-   
         if (other.name == "PlayerModel")
         {
             playerInRange = true;
         }
-        
     }
 
     void OnTriggerExit(Collider other)
@@ -44,7 +54,4 @@ public class Chest : MonoBehaviour
             playerInRange = false;
         }
     }
-
-
-
 }
