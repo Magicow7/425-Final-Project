d76bc980b649a8389512f6a56e5c3a26d4a1e732
Greg <gselez6761@gmail.com>
2024-12-01 20:58:21 -0500

    removed room10 prefab in scene

--- 425 Project/Assets/ButtonHandler.cs
+++ 425 Project/Assets/ButtonHandler.cs
@@ -9,7 +9,8 @@ public class ButtonHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
     // Update is called once per frame
